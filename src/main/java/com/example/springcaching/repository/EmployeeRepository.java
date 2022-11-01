ab0eefbb788a6d5c9068b897ea055f3bb16fea30
mleleszi <mleleszi@icloud.com>
2022-11-01 10:21:56 +0100

    cache all employees

--- src/main/java/com/example/springcaching/repository/EmployeeRepository.java
+++ src/main/java/com/example/springcaching/repository/EmployeeRepository.java
@@ -1,12 +1,22 @@
 package com.example.springcaching.repository;
 
 import com.example.springcaching.entity.Employee;
+import com.example.springcaching.entity.Salary;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.jpa.repository.QueryHints;
+
+import javax.persistence.QueryHint;
 import java.util.List;
+
+import static org.hibernate.jpa.QueryHints.HINT_CACHEABLE;
 
 public interface EmployeeRepository extends JpaRepository<Employee, Integer> {
 
     @Query(value = "select emp_no from employees", nativeQuery = true)
     List<Integer> findAllIds();
+
+    @QueryHints( value = { @QueryHint(name = HINT_CACHEABLE, value = "true")})
+    List<Employee> findAllBy(Pageable pageable);
 }
